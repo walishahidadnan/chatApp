df30b0266dbbd8e2a069cbe8230464da6eb0ed5a
Shahid Adnan Wali <walishahidadnan@gmail.com>
2023-11-08 19:06:12 +0500

    seting chatapp styles and display

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ChatEngine } from 'react-chat-engine';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.css';
 
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ChatEngine 
+        height='100vh'
+        width="100%"
+        projectID= '82c4ec8e-00c7-4d54-bdb8-88e4e88e98a8'
+        userName='ShahidAdnan'
+        userSecret='Kakakhail@2019'
+      />
     </>
   )
 }
